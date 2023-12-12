0ea46d3d6f2571466ad9de6808ca412f3a3b9fa0
Matthew Brooks <mb964033@gmail.com>
2023-12-12 21:44:14 +0000

    update makefile, config file

--- config.lua
+++ config.lua
@@ -1,19 +1,18 @@
 local env = {
 
   name = "santoku-python",
-  version = "0.0.30-1",
+  version = "0.0.31-1",
   variable_prefix = "TK_PYTHON",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.119-1",
   },
 
   test_dependencies = {
-    "inspect >= 3.1.3-0",
-    "luaposix >= 36.2.1-1",
+    "santoku >= 0.0.145-1",
+    "santoku-test >= 0.0.2-1",
     "luassert >= 1.9.0-1",
     "luacov >= 0.15.0-1",
   },
